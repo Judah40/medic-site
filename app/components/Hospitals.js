3bf3a74583c9b386e4c2d03a70fea12d3d39bc4b
Judah40 <judahdore@gmail.com>
2024-06-30 17:42:09 +0000

    updates

--- app/components/Hospitals.js
+++ app/components/Hospitals.js
@@ -6,26 +6,26 @@ export const hospital = [
     name: "Blue Shield",
     location: "1 Waterloo Street,Freetown Sierra Leone",
     phoneNumber: ["+232 76 904707", "+232 33 904707"],
-    image: "/Hospitals/blue-host.webp",
+    image: "./Hospitals/blue-host.webp",
   },
   {
     name: "Choithram Memorial Hospital",
     location: "Hill Station,Freetown Sierra Leone",
     phoneNumber: ["+ 232 22 232598", "+ 232 76 980000"],
-    image: "/Hospitals/choitram.jpeg",
+    image: "./Hospitals/choitram.jpeg",
   },
   {
     name: "Connaught Hospital",
     location: "Wallace Johnson Street,Freetown Sierra Leone",
     phoneNumber: [" + 232 22 222023"],
-    image: "/Hospitals/connaught-hospital.jpeg",
+    image: "./Hospitals/connaught-hospital.jpeg",
   },
   {
     name: "Marie Stopes Health Centre",
     location:
       "10A / 10B Ahmed Drive,Off Sir Samuel Lewis Road,Freetown Sierra Leone",
     phoneNumber: ["+232 76 422810", "+ 232 22 231027"],
-    image: "/Hospitals/marie-stopes.jpeg",
+    image: "./Hospitals/marie-stopes.jpeg",
   },
 ];
 
